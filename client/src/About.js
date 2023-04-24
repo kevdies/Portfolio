8ac971af480e8aaef78c8a4f70ee1841b5cf5e25
Kevin Diesenberg <kdiesenb@gmail.com>
2023-04-24 18:04:53 -0400

    about and projects roughed in

--- client/src/About.js
+++ client/src/About.js
@@ -1,12 +1,46 @@
 import React from "react";
+import { Container, Row, Col } from "reactstrap";
 
-const About = () => {
+function About() {
   return (
-    <div>
-      <h1>About</h1>
-      {/* Your about content goes here */}
-    </div>
+    <Container className="my-5">
+      <h1 className="mb-4 text-center">Hello, I'm Kevin Diesenberg</h1>
+      <Row className="mb-4 text-center">
+        <Col md="4">
+          <img
+            src="/kevin-artichoke.png"
+            alt="Kevin-Artichoke"
+            className="img-fluid rounded mb-4"
+          />
+        </Col>
+        <Col md="4">
+          <img
+            src="/portfolio-picture.png"
+            alt="Lake-Huron"
+            className="img-fluid rounded mb-4"
+          />
+        </Col>
+        <Col md="4">
+          <img
+            src="/kevin-pink-shirt.png"
+            alt="Kevin-Emily"
+            className="img-fluid rounded mb-4"
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col className="text-center">
+          <p className="lead">
+            I am a software engineer with experience in React, Rails, and SQL. I
+            enjoy solving complex problems and learning new technologies.
+          </p>
+          <p>
+            In my free time, I like to read books, go on hikes, and play guitar.
+          </p>
+        </Col>
+      </Row>
+    </Container>
   );
-};
+}
 
 export default About;
--- client/src/Projects.js
+++ client/src/Projects.js
@@ -1,12 +1,108 @@
 import React from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardText,
+  Button,
+} from "reactstrap";
 
-const Projects = () => {
+function Projects() {
   return (
-    <div>
-      <h1>Projects</h1>
-      {/* Your projects content goes here */}
-    </div>
+    <Container className="my-5">
+      <h1 className="mb-4 text-center">My Projects</h1>
+      <Row>
+        <Col md="4">
+          <Card>
+            <CardImg top src="/AWE logo.png" alt="Aweful Skydiving" />
+            <CardBody>
+              <CardTitle>Aweful Skydiving</CardTitle>
+              <CardText>
+                <h5>
+                  A skill-focused event platform for skydivers built with React,
+                  Ruby on Rails, and PostgreSQL.
+                </h5>
+                <ul>
+                  <li>
+                    Created with React, Ruby on Rails, Active Record, Active
+                    Storage, Reactstrap, Bootstrap, and PostgreSQL.
+                  </li>
+                  <li>
+                    Implemented user authentication and authorization using
+                    bcrypt.
+                  </li>
+                  <li>
+                    Utilized Active Record for efficient database queries and
+                    management.
+                  </li>
+                  <li>
+                    Developed a responsive User Interface using Bootstrap and
+                    Reactstrap.
+                  </li>
+                </ul>
+              </CardText>
+              <Button
+                color="primary"
+                href="https://youtu.be/7JUL1CPlHqg"
+                target="_blank"
+              >
+                Demo
+              </Button>{" "}
+              <Button
+                color="secondary"
+                href="https://github.com/kevdies/aweful"
+                target="_blank"
+              >
+                GitHub
+              </Button>
+            </CardBody>
+          </Card>
+        </Col>
+        <Col md="4">
+          <Card>
+            <CardImg top src="/project2.png" alt="Project 2" />
+            <CardBody>
+              <CardTitle>Project 2</CardTitle>
+              <CardText>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+                laoreet tortor ac eros lobortis ultricies.
+              </CardText>
+              <Button color="primary" href="#">
+                Demo
+              </Button>{" "}
+              <Button color="secondary" href="#">
+                GitHub
+              </Button>
+            </CardBody>
+          </Card>
+        </Col>
+        <Col md="4">
+          <Card>
+            <CardImg top src="/project3.png" alt="Project 3" />
+            <CardBody>
+              <CardTitle>Project 3</CardTitle>
+              <CardText>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+                laoreet tortor ac eros lobortis ultricies.
+              </CardText>
+              <Button color="primary" href="#">
+                Demo
+              </Button>{" "}
+              <Button color="secondary" href="#">
+                GitHub
+              </Button>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
-};
+}
 
 export default Projects;
+
+
