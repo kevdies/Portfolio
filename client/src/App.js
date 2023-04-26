97972dc772a5a605b122d769598e9ec6099b0c05
Kevin Diesenberg <kdiesenb@gmail.com>
2023-04-26 15:57:06 -0400

    navbar now collapses when a link is clicked on small devices

--- client/src/App.js
+++ client/src/App.js
@@ -1,71 +1,4 @@
-// import { useState } from "react";
-// import { Link, Routes, Route } from "react-router-dom";
-// import {
-//   Container,
-//   Navbar,
-//   NavbarBrand,
-//   NavbarToggler,
-//   Collapse,
-//   Nav,
-//   NavItem,
-//   NavLink,
-// } from "reactstrap";
-
-// import About from "./About";
-// import Projects from "./Projects";
-// import Contact from "./Contact";
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "./App.css";
-
-// function App() {
-//   const [isOpen, setIsOpen] = useState(false);
-
-//   const toggle = () => setIsOpen(!isOpen);
-
-//   return (
-//     <div>
-//       <Navbar dark expand="md">
-//         <NavbarBrand tag={Link} to="/">
-//           <img src="/logo.png" alt="Logo" width="40" height="40" />
-//         </NavbarBrand>
-//         <NavbarToggler onClick={toggle} />
-//         <Collapse isOpen={isOpen} navbar>
-//           <Nav className="ml-auto" navbar>
-//             <NavItem>
-//               <NavLink tag={Link} to="/">
-//                 About
-//               </NavLink>
-//             </NavItem>
-//             <NavItem>
-//               <NavLink tag={Link} to="/projects">
-//                 Projects
-//               </NavLink>
-//             </NavItem>
-//             <NavItem>
-//               <NavLink tag={Link} to="/contact">
-//                 Contact
-//               </NavLink>
-//             </NavItem>
-//           </Nav>
-//         </Collapse>
-//       </Navbar>
-
-//       <Container fluid>
-//         <Routes>
-//           <Route path="/" element={<About />} />
-//           <Route path="/projects" element={<Projects />} />
-//           <Route path="/contact" element={<Contact />} />
-//         </Routes>
-//       </Container>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link, Routes, Route } from "react-router-dom";
 import {
   Container,
@@ -99,17 +32,17 @@ function App() {
         <Collapse isOpen={isOpen} navbar>
           <Nav className="ml-auto" navbar>
             <NavItem>
-              <NavLink tag={Link} to="/">
+              <NavLink tag={Link} to="/" onClick={toggle}>
                 About
               </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink tag={Link} to="/projects">
+              <NavLink tag={Link} to="/projects" onClick={toggle}>
                 Projects
               </NavLink>
             </NavItem>
             <NavItem>
-              <NavLink tag={Link} to="/contact">
+              <NavLink tag={Link} to="/contact" onClick={toggle}>
                 Contact
               </NavLink>
             </NavItem>
@@ -117,7 +50,7 @@ function App() {
         </Collapse>
       </Navbar>
 
-      <Container fluid style={{ marginTop: "1rem", marginBottom: "1rem" }}>
+      <Container fluid>
         <Routes>
           <Route path="/" element={<About />} />
           <Route path="/projects" element={<Projects />} />
@@ -129,4 +62,3 @@ function App() {
 }
 
 export default App;
-
